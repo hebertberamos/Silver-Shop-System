c93706e0474dfec8a53169ff7a82352529d4ddad
Hebert Benigno <hebertramos09@gmail.com>
2025-01-05 18:17:04 -0300

    first commit

---
+++ backend/src/main/java/com/web/hevepratas/HevepratasApplication.java
@@ -0,0 +1,13 @@
+package com.web.hevepratas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HevepratasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HevepratasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/web/hevepratas/HevepratasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.hevepratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HevepratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
