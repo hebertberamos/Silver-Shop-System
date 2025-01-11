44c40e23303aa87f12cdca5c6d0b234bbe262d44
Hebert Benigno <hebertramos09@gmail.com>
2025-01-11 17:02:38 -0300

    test commit

--- backend/src/main/java/com/web/hevepratas/repositories/UserRepository.java
+++ backend/src/main/java/com/web/hevepratas/repositories/UserRepository.java
@@ -1,11 +1,18 @@
 package com.web.hevepratas.repositories;
 
 import com.web.hevepratas.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u")
+    Page<User> findAllUsers(Pageable pageable);
+
 }
